f2cb7b2bfda9eee640a18fb7b10c227d6f87b5c4
kittipong_B <cartearo@gmail.com>
2017-09-29 10:46:42 +0700

    -update package

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+declare const $: any;
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  actionAlert(){
+    console.log("cycle");
+    $("#carouselExampleIndicators").carousel("cycle");
+  }
+
+
+  ngAfterViewInit(){
+    console.log("hello");
+    $('.your-class').slick({
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 2000,
+    })
+  }
+
+  ngAfterViewChecked() {
+    //let self = this;
+    //UIkit.alert(".nom").close();
+  }
 }
